745b1db4440a505314222e1ac829e45caeac31dd
Dawid Deszcz <d.deszcz@gmail.com>
2023-01-09 19:35:07 +0100

    Initial Project Add

---
+++ src/main/java/guru/springframework/ddsfgpetclinic2/DdSfgPetClinic2Application.java
@@ -0,0 +1,13 @@
+package guru.springframework.ddsfgpetclinic2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdSfgPetClinic2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdSfgPetClinic2Application.class, args);
+    }
+
+}
